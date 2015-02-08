87cfdad41249753ac747d4d2bd94f57098ca9523
Matthew Price <matthew@matthewaprice.com>
2015-02-08 10:31:53 -0700

    added in new functionality and made the parse args be wp_parse_args

--- HTMLTable.php
+++ HTMLTable.php
@@ -1,145 +1,131 @@
 <?php
- 
+
 /**
  * class HTML Table
  *
  * This class will output an html table
- */ 
+ */
 class HTMLTable {
-	
+
 	/**
 	 * Create a default table id in case none is provided
 	 */
 	private static $_table_instance_id = 0;
-	
+
 	/**
 	 * Constructor
 	 *
 	 * @param array
 	 */
 	public function __construct( $args = array() ) {
-		
+
 		self::$_table_instance_id++;
-		$table_id = self::$_table_instance_id;		
-		$defaults = array( 
+		$table_id = self::$_table_instance_id;
+		$defaults = array(
 			'table_id' => $table_id,
 			'table_class' => 'table',
-			'display_footer' => true,
-			'no_rows_message' => 'Nothing Found'
+			'display_footer' => false,
+			'no_rows_message' => 'Nothing Found',
+			'display_table' => true,
+			'limit_rows' => -1
 		);
-		$args = $this->parseArgs( $args,$defaults );
-		extract($args);	
+		$args = wp_parse_args( $args,$defaults );
+		extract($args);
 		$this->table_args = $args;
-			
-	}
-	
-	/**
-	 * This will take arguments and defaults, compare them and then return the revised array
-	 *
-	 * @param array Overrides
-	 * @param array Defaults
-	 */
-	public function parseArgs( $args, $defaults = array() ) {
-	
-		if ( is_object( $args ) ) :
-			$r = get_object_vars( $args );
-		elseif ( is_array( $args ) ) :
-			$r =& $args;
-		else :
-	        $this->parseString($args,$r);
-		endif;  
-		if ( is_array( $defaults ) ) :
-	        return array_merge( $defaults, $r );
-		endif;
-	        
-		return $r;
-	
+
 	}
 
-   /**
-     * used by parseArgs to help parse out the args and defaults
-     *
-     * @param string
-     * @param array
-     * @return array
-     */
-    public function parseString( $string, &$array ) {
-    
-    	parse_str( $string, $array );
-    
-        if ( get_magic_quotes_gpc() ) :
-        	$array = stripslashes_deep($array);
-        endif;
-            
-		return $array;
-    
-    }
-        	
 	/**
 	 * Setup the values for the <th>s in the table
 	 *
 	 * @param array
 	 */
-	public function setHeaders( $headers ) {
-	
+	public function setHeaders( $headers = array() ) {
+
 		$this->table_headers = $headers;
-	
+
 	}
-	
+
 	/**
 	 * Setup the content to be displayed by the table
 	 *
 	 * @param array
 	 */
-	public function setTableContent( $content ) {
-	
+	public function setTableContent( $content = array() ) {
+
 		$this->table_rows = $content;
-	
+
 	}
-	
+
 	/**
 	 * Display the html table
 	 */
 	public function displayTable() {
 
+		if ( !$this->table_args['display_table'] ) {
+			ob_start();
+		}
 		$column_count = count( $this->table_headers );
 		?>
 		<table id="table-<?php echo $this->table_args['table_id']; ?>" class="<?php echo $this->table_args['table_class']; ?>">
 			<thead>
 				<tr>
-					<?php foreach ( $this->table_headers as $h ) : ?>
-						<th><?php echo $h; ?></th>
-					<?php endforeach; ?>
+					<?php
+					foreach ( $this->table_headers as $k => $v ) :
+						if ( !is_array( $v ) ) :
+							?><th><?php echo $v; ?></th><?php
+						else :
+							?><th width="<?php echo $v[0]; ?>"><?php echo $k; ?></th><?php
+						endif;
+					endforeach;
+					?>
 				</tr>
 			</thead>
 			<tbody>
 				<?php if ( !$this->table_rows ) : ?>
 					<tr>
 						<td colspan="<?php echo $column_count; ?>"><?php echo $this->table_args['no_rows_message']; ?></td>
-					</tr>				
+					</tr>
 				<?php else : ?>
+					<?php $i = 1; ?>
 					<?php foreach ( $this->table_rows as $row ) : ?>
 					<tr<?php echo ( isset( $row['row_style'] ) ) ? ' class="' . $row['row_style'] . '"' : ''; ?><?php echo ( isset( $row['row_id'] ) ) ? ' id="' . $row['row_id'] . '"' : ''; ?>>
-					<?php foreach ( range( 1, $column_count ) as $r ) : ?>
+						<?php foreach ( range( 1, $column_count ) as $r ) : ?>
 							<?php $col = 'col' . $r; ?>
 							<td><?php echo $row[$col]; ?></td>
 						<?php endforeach; ?>
-					</tr>	
+					</tr>
+					<?php
+					if ( $this->table_args['limit_rows'] === -1 ) continue;
+					if ( $i == $this->table_args['limit_rows'] ) break;
+					$i++;
+					?>
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</tbody>
 			<?php if ( $this->table_args['display_footer'] ) : ?>
 			<tfoot>
 				<tr>
-					<?php foreach ( $this->table_headers as $h ) : ?>
-						<th><?php echo $h; ?></th>
-					<?php endforeach; ?>
+					<?php
+					foreach ( $this->table_headers as $k => $v ) :
+						if ( !is_array( $v ) ) :
+							?><th><?php echo $v; ?></th><?php
+						else :
+							?><th width="<?php echo $v[0]; ?>"><?php echo $k; ?></th><?php
+						endif;
+					endforeach;
+					?>
 				</tr>
 			</tfoot>
 			<?php endif; ?>
 		</table>
 		<?php
-		
+		if ( !$this->table_args['display_table'] ) {
+			$table = ob_get_contents();
+			ob_end_clean();
+			return $table;
+		}
+
 	}
 
 }
